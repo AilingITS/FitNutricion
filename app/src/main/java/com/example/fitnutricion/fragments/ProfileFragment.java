9c09864fa9e4a30e675896833ec05fca4782276c
Pedro Candia <52978459+AilingITS@users.noreply.github.com>
2021-09-23 17:54:57 -0500

    avance incompleto firebase perfil

--- app/src/main/java/com/example/fitnutricion/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/fitnutricion/fragments/ProfileFragment.java
@@ -4,40 +4,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.fitnutricion.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private TextView perfil_usuario, perfil_nombre, perfil_mail, perfil_password, perfil_edad, perfil_celular;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference dbRef;
 
     public ProfileFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ProfileFragment newInstance(String param1, String param2) {
         ProfileFragment fragment = new ProfileFragment();
         Bundle args = new Bundle();
